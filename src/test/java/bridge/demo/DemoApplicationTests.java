e9b9f2d3fd56c267fbb4b34f92ccbbc51b6abf4f
usnim <sms897@naver.com>
2023-06-18 13:56:33 +0900

    230618 개발환경세팅

---
+++ src/test/java/bridge/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bridge.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
