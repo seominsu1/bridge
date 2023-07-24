3dc5b04c85d76227e5e4fceffc5106c2be6ff9d5
usnim <sms897@naver.com>
2023-07-24 17:30:29 +0900

    cicd 테스트 머지3

--- src/test/java/bridge/demo/DemoApplicationTests.java
+++ src/test/java/bridge/demo/DemoApplicationTests.java
@@ -1,9 +1,8 @@
 package bridge.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
